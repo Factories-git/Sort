f688756d7ff809af4a1d560f535bd8a0fda45536
Factories-git <official.factories@gmail.com>
2024-12-29 12:42:09 +0900

    Changes

--- 신입 사원.py
+++ 신입 사원.py
@@ -11,20 +11,11 @@ for _ in range(int(input())):
     n = int(input())
     for j in range(n):
         document, interview = map(int, input().split())
-        number.append(document + interview)
-        scores.append([document + interview, document, interview])
-        if document == 1 or interview == 1:
-            heapq.heappush(one, [(document + interview), document, interview])
-            if document == interview == 1:
-                heapq.heappush(one, [(document + interview), document, interview])
+        scores.append([document, interview])
     scores.sort()
-    for s, d, i in scores:
-        if d == 1 or i == 1:
+    cutLine = [100001, 100001]
+    for d, i in scores:
+        if cutLine[0] >= d or cutLine[1] >= i:
+            cutLine = [d, i]
             re += 1
-            if d == i == 1:
-                one.append([s, d, i])
-            one.append([s, d, i])
-        else:
-            if (s <= one[1][0] or s <= one[0][0]) and (d < one[1][1] and i < one[0][2]):
-                re += 1
     print(re)
