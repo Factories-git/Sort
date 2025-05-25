2dea3c1d858b86793c93ee530c232aac463bb8c8
Factories-git <official.factories@gmail.com>
2025-05-25 15:14:44 +0900

    Changes

--- 소트.py
+++ 소트.py
@@ -1,10 +1,20 @@
-s = int(input())
-arr = list(map(int, input().split()))
-count = 0
 n = int(input())
-for i in range(n-1):
-    for j in range(n-1-i):
-        if arr[j] < arr[j+1]:
-            arr[j], arr[j+1] = arr[j+1],arr[j]
-            count += 1
-print(arr)
+arr = list(map(int, input().split()))
+s = int(input())
+while True:
+    br = True
+    for i in range(n):
+        max_ = i
+        change = 0
+        for j in range(n-1, i, -1):
+            if arr[max_] < arr[j] and j-i <= s:
+                max_ = j
+                br = False
+                change = j-i
+        if max_ != i:
+            t = arr.pop(max_)
+            arr.insert(i, t)
+            s -= change
+    if br:
+        break
+print(*arr)
